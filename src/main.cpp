aece070e416c59e8e7491116816116e7ce0390d4
lurkydismal <lurkydismal@duck.com>
2023-07-24 06:48:15 -0700

    Fix #19

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,7 @@ int main( int _argumentCount, char* _argumentVector[] ) {
     std::error_code l_exitCode;
 
     try {
-        if ( _argumentCount != 2 ) {
-            fmt::print( stderr, "Usage: {} <port>\n", _argumentVector[ 0 ] );
-
-            l_exitCode.assign( ENOTSUP, std::generic_category() );
-
-            throw( std::runtime_error( l_exitCode.message() ) );
-        }
-
-        uint32_t l_portNumber = std::atoi( _argumentVector[ 1 ] );
-
-        if ( l_portNumber > 65535 ) {
+        if ( ( _argumentCount != 2 ) || ( std::atoi( _argumentVector[ 1 ] ) > 65535 ) ) {
             fmt::print( stderr, "Usage: {} <port>\n", _argumentVector[ 0 ] );
 
             l_exitCode.assign( ENOTSUP, std::generic_category() );
@@ -28,7 +18,7 @@ int main( int _argumentCount, char* _argumentVector[] ) {
 
         asio::io_context l_ioContext;
 
-        host l_host( l_ioContext, l_portNumber );
+        host l_host( l_ioContext, std::atoi( _argumentVector[ 1 ] ) );
 
         l_ioContext.run();
 
