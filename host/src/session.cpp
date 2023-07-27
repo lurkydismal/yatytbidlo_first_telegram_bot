368617da7d3ec7d352113fbc48a82f93bf86003f
lurkydismal <48640375+lurkydismal@users.noreply.github.com>
2023-07-27 04:36:08 -0700

    Close #18 (#20)

--- host/src/session.cpp
+++ host/src/session.cpp
@@ -10,46 +10,60 @@
 
 using asio::ip::tcp;
 
-static bool onReceiveRoutine( JSContext* _context,
-                              std::string _fileName,
-                              const std::string& _response ) {
+static std::error_code onReceiveRoutine( JSContext* _context,
+                                         std::string _fileName,
+                                         const std::string& _response ) {
+    std::error_code l_exitCode;
+
     JS::RootedObject l_global( _context,
                                JavaScriptIC::createGlobal( _context ) );
 
     if ( !l_global ) {
-        return ( false );
+        l_exitCode.assign( EADDRNOTAVAIL, std::generic_category() );
+
+        goto EXIT;
     }
 
-    JSAutoRealm ar( _context, l_global );
+    {
+        JSAutoRealm l_autoRealm( _context, l_global );
 
-    JavaScriptIC::defineFunctions( _context, l_global );
+        if ( !JavaScriptIC::defineFunctions( _context, l_global ) ) {
+            goto EXIT;
+        }
 
-    if ( !JavaScriptIC::executeFileByName( _context, _fileName ) ) {
-        JavaScriptIC::reportAndClearException( _context );
+        AutoReportException l_autoReport( _context );
 
-        return ( false );
+        l_exitCode = JavaScriptIC::executeFileByName( _context, _fileName );
+
+        if ( static_cast< bool >( l_exitCode ) ) {
+            JavaScriptIC::reportAndClearException( _context );
+
+            goto EXIT;
+        }
+
+        {
+            JS::RootedValue l_returnValue( _context );
+            JS::RootedValue l_response( _context );
+            JS::RootedString l_responseAsJSString(
+                _context, JS_NewStringCopyZ( _context, _response.c_str() ) );
+
+            if ( !l_responseAsJSString ) {
+                l_exitCode.assign( EACCES, std::generic_category() );
+
+                goto EXIT;
+            }
+
+            l_response.setString( l_responseAsJSString );
+
+            JavaScriptIC::callGlobalFunction(
+                _context, l_global, "receiveRoutine",
+                JS::HandleValueArray( l_response ), l_returnValue );
+        }
     }
 
-    JS::RootedValue l_returnValue( _context );
-    JS::RootedValue l_response( _context );
-    JS::RootedString l_responseAsJSString(
-        _context, JS_NewStringCopyZ( _context, _response.c_str() ) );
+EXIT:
 
-    if ( !l_responseAsJSString ) {
-        return ( false );
-    }
-
-    l_response.setString( l_responseAsJSString );
-
-    JavaScriptIC::callGlobalFunction( _context, l_global, "receiveRoutine",
-                                      JS::HandleValueArray( l_response ),
-                                      l_returnValue );
-
-    // JavaScriptIC::executeCode( _context, "noname", 1, R"js(
-    //     print( `hello world, it is ${new Date()}` );
-    // )js" );
-
-    return ( true );
+    return ( l_exitCode );
 }
 
 session::session( tcp::socket _socket ) : socket_( std::move( _socket ) ) {}
@@ -64,31 +78,44 @@ void session::doRead( void ) {
     socket_.async_read_some(
         asio::buffer( data_, MAX_LENGTH ),
         [ this, self ]( std::error_code _errorCode, std::size_t _length ) {
-            if ( !_errorCode ) {
+            if ( _errorCode ) {
+                errorCode_ = errorCode_;
+            } else {
                 fmt::print( fmt::emphasis::bold | fg( fmt::color::green ),
                             "{}\n", data_ );
                 fmt::print( fg( fmt::color::white ), "\n" );
 
                 if ( !JS_Init() ) {
-                    return;
+                    errorCode_.assign( EADDRNOTAVAIL, std::generic_category() );
+
+                    goto EXIT;
                 }
 
-                JSContext* l_context = JS_NewContext( JS::DefaultHeapMaxBytes );
+                {
+                    JSContext* l_context =
+                        JS_NewContext( JS::DefaultHeapMaxBytes );
 
-                if ( !l_context ) {
-                    return;
+                    if ( !l_context ) {
+                        errorCode_.assign( EADDRNOTAVAIL,
+                                           std::generic_category() );
+
+                        goto DESTROY_CONTEXT_AND_SHUTDOWN;
+                    }
+
+                    if ( !JS::InitSelfHostedCode( l_context ) ) {
+                        errorCode_.assign( EACCES, std::generic_category() );
+                    }
+
+                    errorCode_ =
+                        onReceiveRoutine( l_context, fileName_, data_ );
+
+                DESTROY_CONTEXT_AND_SHUTDOWN:
+
+                    JS_DestroyContext( l_context );
+                    JS_ShutDown();
                 }
 
-                if ( !JS::InitSelfHostedCode( l_context ) ) {
-                    return;
-                }
-
-                if ( !onReceiveRoutine( l_context, fileName_, data_ ) ) {
-                    return;
-                }
-
-                JS_DestroyContext( l_context );
-                JS_ShutDown();
+            EXIT:
 
                 doWrite( _length );
             }
@@ -101,7 +128,9 @@ void session::doWrite( std::size_t _length ) {
     asio::async_write(
         socket_, asio::buffer( data_, _length ),
         [ this, self ]( std::error_code _errorCode, std::size_t /*_length*/ ) {
-            if ( !_errorCode ) {
+            if ( _errorCode ) {
+                errorCode_ = _errorCode;
+            } else {
                 // fmt::print( fmt::emphasis::bold | fg( fmt::color::blue ),
                 //             "{}\n", data_ );
                 // fmt::print( fg( fmt::color::white ), "\n" );
